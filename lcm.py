6d7360a42d65228bbc5f63f7505c39d556b94112
NITESH KUMAR TIWARI <NITESHTIWARI4733@GMAIL.COM>
2026-05-23 19:43:52 +0530

    corrected

--- lcm.py
+++ lcm.py
@@ -1,11 +1,11 @@
 def lcm(a,b):
     greater = max(a,b)
-   while True:
+    while True:
      if greater % a==0 and greater % b==0:
-       return greater 
+       return greater
      greater +=1
 
-x = int(input("Enter the first number:")
-y= int (intput("Enter the second number:")
+x = int (input("Enter the first number:"))
+y= int (input("Enter the second number:"))
 print(f"the LCM of {x} and {y} is :{lcm(x,y)}")
-        
+
