efb9ccf3c40c1c8de5760e319b47c8dc5a1d88e9
NITESH KUMAR TIWARI <NITESHTIWARI4733@GMAIL.COM>
2026-05-25 10:36:59 +0530

    3 more

---
+++ reverse_string.py
@@ -0,0 +1,3 @@
+text =input("enter a string ")
+reversed_text= text[::-1]
+print(f"Reversed string:{reversed_text}")
---
+++ sum_of_digits.py
@@ -0,0 +1,3 @@
+num = int(input("enter the number:"))
+sum_of_digit= sum (int(digit) for digit in str(num))
+print(f"the sum of the digits is :{sum_of_digit}")
