1de9f111cb5f434b0af8e08e5183c7497fa01f03
NITESH KUMAR TIWARI <NITESHTIWARI4733@GMAIL.COM>
2026-05-29 22:45:32 +0530

    26 to 30

---
+++ calculat_sum_of_list.py
@@ -0,0 +1,3 @@
+numbers = list(map(int, input("Enter numbers separated by spaces:").split()))
+total= sum(numbers)
+print(f"The sum of the number is :{total}")
---
+++ calculate_avg_number.py
@@ -0,0 +1,3 @@
+numbers= list(map(int, input("Enter number seprated by spaces:").split()))
+average= sum(numbers)/len(numbers)
+print(f"The average of the numbers is :{average:.2f}")
